2007ef110128369a2336c78bafc1319579fbd061
Papa Fall <papa.fall1@gmail.com>
2024-01-27 20:27:28 -0800

    Remove function arguments from array using filter an includes

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,6 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (Arr1, ...goner) {
+  const newArr = Arr1.filter((item) => !goner.includes(item));
+  return newArr;
 };
 
 // Do not edit below this line
