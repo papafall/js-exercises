fe6313bfa24589ef2255ec0a7236d965804f8caf
Papa Fall <papa.fall1@gmail.com>
2024-01-27 21:13:51 -0800

    Created function that takes 2 integers and returns the sum of every number between(and including) them

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function (start, end) {
+  let finalSum = 0;
+  if (
+    start < 0 ||
+    end < 0 ||
+    typeof start !== "number" ||
+    typeof end !== "number"
+  ) {
+    return "ERROR";
+  } else if (start > end) {
+    [start, end] = [end, start];
+  }
+  for (let i = start; i <= end; i++) {
+    finalSum += i;
+  }
+  return finalSum;
 };
 
 // Do not edit below this line
