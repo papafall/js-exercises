b7f194542357d60a7b0205c567bf4ec62efe551c
Papa Fall <papa.fall1@gmail.com>
2024-01-27 23:26:45 -0800

    Wrote functions to convert Fahr to Cels and C to F

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (Fahr) {
+  const Cels = (Fahr - 32) * (5 / 9);
+  const CelsRounded = Math.round(Cels * 10) / 10;
+  return CelsRounded;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (tempC) {
+  const tempF = tempC * (9 / 5) + 32;
+  const tempFRounded = Math.round(tempF * 10) / 10;
+  return tempFRounded;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
